bcbad9c2b6436bac0f94bed2dcb14a36021f52b5
HussainWorld <164430022+HussainWorld@users.noreply.github.com>
2025-10-27 23:11:29 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1><h2>Hi, I’m Hussain 👋</h2></h1>
+      <div className="card">
+        <p>Welcome to my protfolio website</p>
+      </div>
+    </>
+  )
+}
+
+export default App
