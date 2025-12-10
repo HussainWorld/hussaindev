afa3f3e4b71d852cf873a852b34f19a36fd6d27e
HussainWorld <164430022+HussainWorld@users.noreply.github.com>
2025-12-11 00:34:55 +0300

    enhancing the speed of the website and redusing the size of the images

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Hi, I’m Hussainnnnnnnnn 👋🏻</h1>
+      <h1>Hi, I’m Hussain 👋🏻</h1>
       <div className="card">
         <p>Welcome to my portfolio website</p>
       </div>
